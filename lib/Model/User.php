0b4e420ae606911b05b9a2e863bb9f8f02b6adf1
Romans Malinovskis <r@agiletech.ie>
2013-02-08 16:38:31 +0000

    rename file

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -1,11 +1,10 @@
 <?php 
 
-class Model_Users extends Model_Table {
+class Model_User extends Model_Table {
 	public $table = 'users';
 
     function init(){
         parent::init();
-
        
         $this->addField('name');
         $this->addField('email');
